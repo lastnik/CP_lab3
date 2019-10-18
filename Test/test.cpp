aa5b5eb59a844368dbdfdde3734f1366e99302e5
Lastochkin <nikita.lastochkin.ext@nokia.com>
2019-10-18 12:13:42 +0300

    Init Rsa and Test framework

---
+++ Test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <gtest/gtest.h>
+TEST( HELLO, WORLD)
+{
+    EXPECT_EQ(true, true);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
